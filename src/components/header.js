fff8c83ba2cc3010aee27669d6940c79b69c3251
Isabel Brinkmann <isabel95estrada@hotmail.com>
2024-05-15 16:15:44 -0600

    just some changes

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,24 @@ import Nav from './nav';
 
 function Header() {
     return (
-        <header>
-            <img src="icons_assets/logo.svg" alt="logo"/>
+        <header className="Header">
+            <div className="navbar">
+            <img src="/Logo.svg" alt="Logo" />
             <Nav/>
+            </div>
+            <div className="header_container">
+                <div className="header_content">
+                    <div className="header_content_text">
+                        <h1 className="header_content_text_title">Little Lemon</h1>
+                        <h2 className="header_content_text_subtitle">Chicago</h2>
+                        <p className="header_content_text_parragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <button type="submit" className="header_content_text_button">Reserve a table</button>
+                    </div>
+                    <div className="Header_content_picture">
+                        <img src="/bruchetta.svg"></img>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 }
--- src/components/main.js
+++ src/components/main.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Main() {
     return (
-        <header>
-            <h1>Titulo del Sitio</h1>
-        </header>
+        <main className="main">
+            
+        </main>
     );
 }
 
